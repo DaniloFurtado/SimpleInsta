a56c52cd4a529a70434fab3738540662705ca3b7
danilo <danilodesenv@gmail.com>
2019-01-25 04:46:52 +0000

    Removed the class mock, and using now all mocks by mockito

--- app/src/test/java/desenv/danilo/simpleinsta/ui/auth/AuthenticationViewModelTest.kt
+++ app/src/test/java/desenv/danilo/simpleinsta/ui/auth/AuthenticationViewModelTest.kt
@@ -1,8 +1,14 @@
 package desenv.danilo.simpleinsta.ui.auth
 
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import desenv.danilo.simpleinsta.data.ui.auth.AuthenticationRepository
 import desenv.danilo.simpleinsta.data.ui.auth.AuthenticationViewModel
+import io.reactivex.Single
 import io.reactivex.observers.TestObserver
 import io.reactivex.schedulers.TestScheduler
+import org.mockito.ArgumentMatchers
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import java.lang.Exception
@@ -13,54 +19,76 @@ class AuthenticationViewModelTest: Spek({
 
     val testScheduler = TestScheduler()
 
+
     describe("Start Authentication"){
-        val repositoryMock = AuthenticationRepositoryMock()
+        val urlLogin = "https://api.instagram.com/oauth/authorize/?client_id=724a4916ffab473f995b91826b5c3f53&redirect_uri=https://instagram.com/&response_type=token&display=touch&scope=public_content"
+        val repositoryMock = mock<AuthenticationRepository>{
+            on{extractToken(ArgumentMatchers.anyString())} doReturn Single.just(urlLogin)
+        }
         val authViewModel = AuthenticationViewModel(repositoryMock, testScheduler, testScheduler)
         val testObserver = TestObserver<String>()
-        val urlLogin = "https://api.instagram.com/oauth/authorize/?client_id=724a4916ffab473f995b91826b5c3f53&redirect_uri=https://instagram.com/&response_type=token&display=touch&scope=public_content"
         it("Must hide the button login and send a url to view") {
             authViewModel.loadInstaPage.subscribe(testObserver)
             authViewModel.startAuthenticate()
             testScheduler.triggerActions()
             testObserver.assertValue(urlLogin)
             assertEquals(false, authViewModel.showButtonLogin.get())
+            testObserver.dispose()
         }
     }
 
 
     describe("After authentication"){
-        val repositoryMock = AuthenticationRepositoryMock()
-        val authViewModel = AuthenticationViewModel(repositoryMock, testScheduler, testScheduler)
+
         it("When has been success Must hide progress and notify view of success") {
+
+            val repositoryMock = mock<AuthenticationRepository>{
+                on { registerToken(ArgumentMatchers.anyString()) } doReturn Single.just(true)
+                on { extractToken(ArgumentMatchers.anyString()) } doReturn Single.just("")
+            }
+            val authViewModel = AuthenticationViewModel(repositoryMock, testScheduler, testScheduler)
             val testObserver = TestObserver<Boolean>()
             authViewModel.authenticatedSucess.subscribe(testObserver)
-            authViewModel.registerToken("")
+            authViewModel.registerToken(ArgumentMatchers.anyString())
             testScheduler.triggerActions()
             testObserver.assertValue(true)
             assertEquals(false, authViewModel.showProgress.get())
+            testObserver.dispose()
         }
 
         it("When happen a error, must notify the view hide the progress and show login button") {
             val testObserver = TestObserver<String>()
-            repositoryMock.exception = Exception("Error requesting token")
+            val repositoryMock = mock<AuthenticationRepository>{
+                on { registerToken(ArgumentMatchers.anyString()) } doReturn Single.error(Exception("Error requesting token"))
+                on { extractToken(ArgumentMatchers.anyString()) } doReturn Single.just("")
+            }
+
+            val authViewModel = AuthenticationViewModel(repositoryMock, testScheduler, testScheduler)
+
             authViewModel.publishError.subscribe(testObserver)
-            authViewModel.registerToken("")
+            authViewModel.registerToken(ArgumentMatchers.anyString())
             testScheduler.triggerActions()
             testObserver.assertValue("Error requesting token")
             assertEquals(false, authViewModel.showProgress.get())
             assertEquals(true, authViewModel.showButtonLogin.get())
+            testObserver.dispose()
         }
 
         it("When happen a error because return false, must notify the view hide the progress and show login button") {
             val testObserver = TestObserver<String>()
-            repositoryMock.responseLogout = false
-            repositoryMock.exception = null
+            val repositoryMock = mock<AuthenticationRepository>{
+                on{registerToken(ArgumentMatchers.anyString())} doReturn Single.just(false)
+                on { registerToken("isso vai ") } doReturn  Single.just(false)
+                on{extractToken(ArgumentMatchers.anyString())} doReturn Single.just("")
+            }
+            val authViewModel = AuthenticationViewModel(repositoryMock, testScheduler, testScheduler)
             authViewModel.publishError.subscribe(testObserver)
-            authViewModel.registerToken("")
+            authViewModel.registerToken(ArgumentMatchers.anyString())
             testScheduler.triggerActions()
             testObserver.assertValue("Error fetching the token")
             assertEquals(false, authViewModel.showProgress.get())
             assertEquals(true, authViewModel.showButtonLogin.get())
+            testObserver.dispose()
         }
     }
 })
