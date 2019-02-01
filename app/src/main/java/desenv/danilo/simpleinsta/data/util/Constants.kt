e65216de33b08f7483e381ec2fa17cb0a8de1bcb
Danilo Furtado <danilodesenv@gmail.com>
2019-02-01 01:11:59 +0000

    Update Constants.kt
    hidded my instagrams client

--- app/src/main/java/desenv/danilo/simpleinsta/data/util/Constants.kt
+++ app/src/main/java/desenv/danilo/simpleinsta/data/util/Constants.kt
@@ -1,8 +1,8 @@
 package desenv.danilo.simpleinsta.data.util
 
 object Constants {
-    val INSTAGRAM_CLIENTE_ID: String = "724a4916ffab473f995b91826b5c3f53"
-    val INSTAGRAM_CLIENTE_ID_SECRET: String = "bee6d14a80f94a69a872afdc2a7e5f40"
+    val INSTAGRAM_CLIENTE_ID: String = "put you instagram client id"
+    val INSTAGRAM_CLIENTE_ID_SECRET: String = "put your instagram client secret"
     val GET_USER_INFO_URL = "v1/users/self/?access_token="
 
     val BASE_URL: String = "https://api.instagram.com/"
