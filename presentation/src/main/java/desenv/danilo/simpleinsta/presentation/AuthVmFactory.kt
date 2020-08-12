cd8b80d89499fd41fb3b11b942faf17a29ed835c
Danilo Furtado <danilodesenv@gmail.com>
2020-08-12 00:20:07 -0300

    nao vai

--- presentation/src/main/java/desenv/danilo/simpleinsta/presentation/AuthVmFactory.kt
+++ presentation/src/main/java/desenv/danilo/simpleinsta/presentation/AuthVmFactory.kt
@@ -6,11 +6,14 @@ import desenv.danilo.simpleinsta.presentation.di.DaggerPresentationComponent
 
 class AuthVmFactory: ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
+
         return when {
             modelClass.isAssignableFrom(AuthViewModel::class.java) ->
                 DaggerPresentationComponent.builder().build().initAuthViewModel() as T
 
              else -> throw IllegalArgumentException("ViewModel Not Found")
         }
+
     }
+
 }
